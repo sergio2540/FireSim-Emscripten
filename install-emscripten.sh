644c5cf6fbc0624c8ae118cc80f70e740fb68721
Sérgio Alves <sergio2540@gmail.com>
2013-08-15 00:28:56 +0100

    update install-emscripten

--- install-emscripten.sh
+++ install-emscripten.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get install git subversion cmake build-essential default-jre scons -y
-cd ~
-svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_32/final llvm32
-cd llvm32/tools
-svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_32/final clang
-cd ../..
-mkdir llvm32build
-cd llvm32build
-cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ../llvm32
-make
-cd ~
-echo "export PATH=~/llvm32build/bin:\$PATH" >> .bashrc
-cd ~
+sudo apt-get install git subversion cmake build-essential default-jre scons clang -y
 clang --version
 rm ~/.emscripten
 rm -rf ~/.emscripten_cache
