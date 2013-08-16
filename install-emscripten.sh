8b57451d688ed4521a29fd126613c035ff77206b
Sérgio Alves <sergio2540@gmail.com>
2013-08-16 12:04:58 +0100

    update install-emscripten.sh

--- install-emscripten.sh
+++ install-emscripten.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get install git subversion cmake build-essential default-jre clang-3.2 scons -y
+sudo apt-get install git subversion cmake build-essential default-jre scons -y
 wget http://llvm.org/releases/3.2/clang+llvm-3.2-x86-linux-ubuntu-12.04.tar.gz
 tar -xvf clang+llvm-3.2-x86-linux-ubuntu-12.04.tar.gz
 cd clang+llvm-3.2-x86-linux-ubuntu-12.04
@@ -9,12 +9,14 @@ sudo cp -R ./bin/* /usr/bin
 sudo cp -R ./include/* /usr/include
 sudo cp -R ./lib/* /usr/lib
 sudo cp -R ./lib/* /usr/share
+cd ..
 cd ./emscripten
 git checkout master
 ./emcc --help
 cd ..
+make install
 make c CC=cc
-make run-c
+time make run-c
 make cp EMCC=../emscripten/emcc
 cd ./crowdprocess/build 
 ls
