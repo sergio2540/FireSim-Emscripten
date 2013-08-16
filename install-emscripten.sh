258487ad897891358938ffcf1671647fe14d164e
Sérgio Alves <sergio2540@gmail.com>
2013-08-16 11:22:29 +0100

    install-emscripten.sh

--- install-emscripten.sh
+++ install-emscripten.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get install git curl subversion cmake build-essential default-jre scons -y
+sudo apt-get install git curl subversion cmake build-essential default-jre clang-3.2 scons -y
 clang --version
 cd ./emscripten
 git checkout master
 ./emcc --help
 ./emcc tests/hello_world.cpp
+ls
 node a.out.js
