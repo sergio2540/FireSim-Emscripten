3f1f0a5e77b6db59ed6cbd1584ebc58ed45207bf
Sérgio Alves <sergio2540@gmail.com>
2013-08-16 11:12:58 +0100

    update install-emscripten.sh

--- install-emscripten.sh
+++ install-emscripten.sh
@@ -2,13 +2,7 @@
 
 
 sudo apt-get install git curl subversion cmake build-essential default-jre scons -y
-SCRIPT_URL=https://gist.github.com/shovon/6151878/raw/62c4f4b5733e772b5a627aebd2b0b32d7cd4ab1e/gistfile1.sh
-mkdir llvm-src
-cd llvm-src
-curl $SCRIPT_URL | sh
 clang --version
-rm ~/.emscripten
-rm -rf ~/.emscripten_cache
 cd ./emscripten
 git checkout master
 ./emcc --help
